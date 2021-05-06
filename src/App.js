bf2a0bd88b87269d6a01186857bbb393534f08cd
SiEr0212 <simonertel@gmx.de>
2021-05-06 16:14:54 +0200

    added useCustomFetch logic

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
+const getFolowers = (e) => {
+if(e.key === 'Enter'){
+  alert('enter pressed')
+}
+};
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h2>
+          GitID:
+          <input onKeyPress={getFolowers}></input>
+        </h2>
+      </header>
     </div>
   );
 }
---
+++ src/hooks/useCustomFetch.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+export default function useCustomFetch(url) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const customFetch = async (url) => {
+    try {
+      let response = await fetch(url);
+      let resData = await response.json();
+      setData({ resData });
+      setLoading(false);
+    } catch (e) {
+      setError(e);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      customFetch(url);
+    }, 3000);
+  }, [url]);
+}
